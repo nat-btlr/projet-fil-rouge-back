6dadb05c8c26e7ab5279e858fa60f8c0111aeef5
Natalia Sugrobova <natalyasugrobova@gmail.com>
2025-05-06 15:17:56 +0200

    Checking if the project is functioning before working on video display

--- src/main/java/fr/filrougeback/repository/VideoRepository.java
+++ src/main/java/fr/filrougeback/repository/VideoRepository.java
@@ -1,11 +1,10 @@
 package fr.filrougeback.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import fr.filrougeback.model.User;
+import fr.filrougeback.model.Video;
 
 @Repository
-public interface VideoRepository extends CrudRepository<User, Integer>{
-
+public interface VideoRepository extends JpaRepository<Video, Integer>{
+	void deleteById(Integer idVideo);
 }
--- src/main/java/fr/filrougeback/service/VideoService.java
+++ src/main/java/fr/filrougeback/service/VideoService.java
@@ -1,13 +1,57 @@
 package fr.filrougeback.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.filrougeback.model.Video;
 import fr.filrougeback.repository.VideoRepository;
 
 @Service
 public class VideoService {
 	@Autowired
 	VideoRepository videoRepository;
+	
+	public List<Video> findAll() {
+		List<Video> videos = videoRepository.findAll();
+		return videos;
+	}
+	
+	public Video addVideo(String title, String description, String url) {
+		Video video = new Video();
+		video.setTitle(title);
+		video.setDescription(description);
+		video.setUrl(url);
+		return videoRepository.save(video);
+	}
+	
+	public Video updateVideo(Video video, String newTitle, String newDescription, String newUrl) {
+		if (video != null) {
+			System.out.println("Video found");
+			System.out.println(video);
+			
+            if (newTitle != null && !newTitle.isBlank()) {
+                video.setTitle(newTitle);
+                System.out.println("Title updated" + newTitle);
+            }
+            if (newDescription != null && !newDescription.isBlank()) {
+                video.setDescription(newDescription);
+            }
+            if (newUrl != null && !newUrl.isBlank()) {
+                video.setUrl(newUrl);
+            }
 
+            return videoRepository.save(video);
+        } else {
+        	System.out.println("Video null");
+        	return null;
+        }
+	}
+	
+	public void deleteVideoByTitle(String Title) {
+		
+	}
 }
+
+
